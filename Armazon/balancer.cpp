091b95631d346b59b6067d04f0263fce6d799785
TheLopezepoL <thelopezepol@outlook.es>
2020-06-05 16:50:48 -0600

    Balancer Try

--- Armazon/balancer.cpp
+++ Armazon/balancer.cpp
@@ -1,6 +1,24 @@
 #include "balancer.h"
 
-Balancer::Balancer()
-{
+Balancer::Balancer() {}
 
+void Balancer::__init__(){
+    pause = false;
+    play = true;
+}
+
+void Balancer::Pause(){
+    pause = true;
+}
+
+void Balancer::Unpause(){
+    pause = false;
+}
+
+void Balancer::run(){
+    while (play){
+        if (pause)
+            sleep(1);
+
+    }
 }
--- Armazon/balancer.h
+++ Armazon/balancer.h
@@ -1,9 +1,27 @@
 #ifndef BALANCER_H
-#define BALANCER_H
+#define
+#include <QThread>
+#include "orderqueue.h"
 
 //ESTRUCTURA DEL BALANCEADOR
-struct Balancer {
+class Balancer : public QThread {
+
+public:
+    OrderQueue pedidos;
+    OrderQueue fabricar;
+    OrderQueue alisto;
+
+    bool play;
+    bool pause;
+
     Balancer();
+
+    void __init__();
+    void run();
+    void Pause();
+    void Unpause();
+
+
 };
 
 #endif // BALANCER_H
