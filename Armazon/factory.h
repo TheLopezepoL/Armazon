dff690f4ddb5e133bb9ee0838761683dd7f67741
TheLopezepoL <thelopezepol@outlook.es>
2020-06-15 15:07:08 -0600

    Try3

--- Armazon/factory.h
+++ Armazon/factory.h
@@ -3,11 +3,11 @@
 #include <QThread>
 #include "orderqueue.h"
 #include "articlelist.h"
-
+#include <QDateTime>
 
 class Factory : public QThread {
 public:
-    //Category category;
+    Category category;
     OrderQueue* balancerQueue;
     bool play;
     bool pause;
@@ -16,12 +16,14 @@ public:
 
     Factory();
 
-    void __init__(/*Category category,*/ OrderQueue* balancerQueue, ArticleList* articles,OrderQueue* first, OrderQueue* second = nullptr);
+    void __init__(Category category, OrderQueue* balancerQueue, ArticleList* articles,OrderQueue* first, OrderQueue* second = nullptr);
     void run();
     int biggerQueue();
     void fabricate(NodeOrder* node);
     void Pause();
     void Unpause();
+
+    QString categoryName();
 };
 
 #endif // FACTOR_H
