eb2438a2d692aaeeb602ff8cfbc14f57c3c1a909
TheLopezepoL <thelopezepol@outlook.es>
2020-06-16 15:25:33 -0600

    prueba2

--- Armazon/vault.h
+++ Armazon/vault.h
@@ -1,11 +1,19 @@
 #ifndef VAULT_H
 #define VAULT_H
+#include "recrutier.h"
 
 //ALMACEN
 struct Vault {
+    Article* vault[10][27];
+    Recrutier* actives[6];
+    Recrutier* inactives[6];
+
+    Vault(Article* vault[10][27]);
+
+    void alocateArticles(ArticleList* articles);
+    int calculateTime(QString pos);
 
 
-    Vault();
 };
 
 #endif // VAULT_H
