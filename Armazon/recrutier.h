1621daec30a0cd25202d5493a5b2bd7af80395a6
revv-tech <52094428+revv-tech@users.noreply.github.com>
2020-06-17 15:03:26 -0600

    Update recrutier.h

--- Armazon/recrutier.h
+++ Armazon/recrutier.h
@@ -3,22 +3,26 @@
 #include <QThread>
 #include "orderqueue.h"
 #include "articlelist.h"
+#include <QDateTime>
 
 
 //ALISTADOR QUE RECORRE EL ALAMACEN RECOGIENDO LOS ARTICULOS SOLICITADOS POR EL PEDIDO
-class Recrutier : public QThread{
+class Recrutier : public QThread{   // Guardar en un array en Armazon
     bool play;
     bool pause;
-    OrderQueue* queue;
+    OrderQueue* arriving;
+    OrderQueue* outgoing;
+    ArticleList* articles;
     int id;
     bool active;
 
     Recrutier();
 
-    void __init__(OrderQueue* queue, int id);
+    void __init__(OrderQueue* arriving, OrderQueue* outgoing, ArticleList* articles, int id);
     void run();
     void Pause();
     void Unpause();
+    int calculateTime(NodeOrder* order, Request* request);
 };
 
 #endif // RECRUTIER_H
